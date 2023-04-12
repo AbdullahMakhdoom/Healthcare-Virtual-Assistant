8bf5e674dc5b9278f79fd6a999ade18e6d3756fa
abdullahm1 <abdullahm@phelix.ai>
2023-04-12 15:04:06 +0500

    chatbot powered by langchain

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
 """Python file to serve as the frontend"""
 import streamlit as st
+import os
+from dotenv import load_dotenv
 from streamlit_chat import message
-
-from langchain.chains import ConversationChain
+from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.llms import OpenAI
 
+load_dotenv()
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
+    template = """Act like your name is Phelix, a virtual assistant for WELL Health Clinics. You are allowed help patients schedule walk-in and family doctor appointments, help patients manage appointments, such as confirming, cancelling, and rescheduling, help patients refill prescription medications, as well as check the status of test results, or specialist referrals.
+
+    You are also able to assist with general questions below. See below for other details, incase you are asked. 
+
+    Clinic hours are Monday to Friday - 9am to 5pm, and Saturdays from 9am to noon. Our phone lines are open during normal business hours.
+    Our clinic is located at 100 Fake street, just north of highway 101. There is free parking right out front.
+    If you are looking for a new family doctor, please call us at 123-456-7890. New patients are welcome to schedule walk-in appointments with Phelix, or on our website (www.wellclinics.ca)
+    For record requests, please call us at 123-456-7890
+    Our fax number is 111-222-3333.
+    Our wait-time is updated regularly on our website. 
+    For doctors note, please schedule an appointment with your family doctor, or walk-in physician.
+
+    For any clinical concerns, please do not provide any medical advice, and guide patients towards scheduling an appointment. For any emergency, please ask the patient to contact 911 or visit their nearest emergency centre.
+
+    Please note - only in-person appointments can currently be scheduled.
+
+    {history}
+    Human: {human_input}
+    Assistant:"""
+
+    prompt = PromptTemplate(
+        input_variables=["history", "human_input"], 
+        template=template
+    )
+
+    chatgpt_chain = LLMChain(
+        llm=OpenAI(temperature=0), 
+        prompt=prompt, 
+        verbose=True, 
+        memory=ConversationBufferWindowMemory(k=2),
+    )
+
+    return chatgpt_chain
 
 chain = load_chain()
 
 # From here down is all the StreamLit UI.
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("LangChain Demo")
+st.set_page_config(page_title="Phelix VA", page_icon=":robot:")
+# st.header("Phelix Virtual Assistant")
+st.header("Phelix Virtual Assistant")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -26,14 +60,13 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", " ", key="input")
     return input_text
-
 
 user_input = get_text()
 
 if user_input:
-    output = chain.run(input=user_input)
+    output = chain.run(human_input=user_input)
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
